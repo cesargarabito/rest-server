3cdb5bc8cdd971235e56773862cd4c35feec63af
Cesar Garabito <cesargarabito97@gmail.com>
2023-06-14 18:57:23 +0200

    se crea todo lo referente a google

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { Router } = require('express');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 
 const router = Router();
 
@@ -10,6 +10,11 @@ router.post('/login', [
     check('password', 'La contranesa no es valida').not().isEmpty(),
     validarCampos
 ],login)
+
+router.post('/google', [
+    check('id_token', 'El id_token de Google es necesario').not().isEmpty(),
+    validarCampos
+],googleSignIn)
 
 
 module.exports = router;
